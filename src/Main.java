6903ab0a015f833412c9a3b2b1587fdb14b6f190
shibuya-n <a.duong2006@gmail.com>
2023-09-19 16:38:10 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 public class Main {
+    public static String output;
 
     public static void main(String[] args) {
+        ask();
+    }
+
+    public static void ask(){
         Scanner inputType = new Scanner(System.in);
         System.out.println("Which type of unit are you converting from?");
         System.out.println("Binary/Hex/Decimal");
@@ -13,39 +18,58 @@ public class Main {
         Scanner outputType = new Scanner(System.in);
         System.out.println("Which type of unit are you converting to?");
         System.out.println("Binary/Hex/Decimal");
-        String output = outputType.nextLine().strip().toLowerCase();
+        output = outputType.nextLine().strip().toLowerCase();
         System.out.println("Mode: " + input + " to " + output);
 
         if (input.equals("binary")) {
-            Scanner binaryInput = new Scanner(System.in);
-            System.out.println("Insert a binary code (eight numbers)");
-            String binary = binaryInput.nextLine();
+            binaryAsk();
+
+
+
+        }
+    }
+
+    public static void binaryAsk(){
+        Scanner binaryInput = new Scanner(System.in);
+        System.out.println("Insert a binary code (eight numbers)");
+        String binary = binaryInput.nextLine();
+
+        if (binary.length() == 8) {
             if (output.equals("decimal")) {
 
-                binaryToDecimal(binary);
+                System.out.println("Total is: " + binaryToDecimal(binary));
+                ask();
+
             }
+            else if (output.equals("hex")){
+
+                int decimal = binaryToDecimal(binary);
+                binaryToHex(decimal);
+            }
+        }
+        else {
+            System.out.println("You did not enter eight digits.");
+            binaryAsk();
 
         }
     }
 
 
-    public static void binaryToDecimal(String input) {
+    public static int binaryToDecimal(String input) {
         int total = 0;
         int j = 0;
 
-        for (int i = 0; i < input.length(); i++) {
-            String num = input.substring(i-i, i-);
+
+
+        for (int i = input.length()-1; i >= 0; i--) {
+            String num = input.substring(i, i+1);
 
             int x = Integer.parseInt(num);
-            System.out.print(x);
-
-
-
 
 
             if (x == 1) {
-                total += Math.pow(x, j);
-                System.out.println(total);
+                total += Math.pow(2, j);
+
                 j++;
 
             }
@@ -54,7 +78,11 @@ public class Main {
             }
 
         }
-        System.out.println("Result: " + total);
+
+        return total;
+    }
+    public static String binaryToHex(int input){
+
     }
 }
 
